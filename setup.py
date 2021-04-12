109472a7691e08818fc6bb372ef65fe3a814d1ea
Guilherme Paulino <guilherme.paulino@pitec.co>
2021-04-11 21:08:17 -0300

    cookiecutter template for data science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Disciplina de Ciência e Visualização de Dados em Saúde (2021.1)',
+    author='UNICAMP',
+    license='MIT',
+)
